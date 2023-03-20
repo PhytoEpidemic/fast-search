8e6d2821094cde885cf9c7fee236628c91a7abfa
PhytoEpidemic <64293310+PhytoEpidemic@users.noreply.github.com>
2023-03-20 18:52:43 -0500

    Sort

--- search.ps1
+++ search.ps1
@@ -9,9 +9,98 @@ $toolTip.AutoPopDelay = 10000
 	
 Return $toolTip
 }
+
+function Sort-ListBoxAlphabetically {
+    param([System.Windows.Forms.ListBox]$listbox)
+
+    # Get the items in the listbox as an array
+    $itemsArray = @($listbox.Items)
+
+    # Sort the items alphabetically
+    $sortedItemsArray = $itemsArray | Sort-Object
+
+    # Clear the listbox and add the sorted items
+    $listbox.Items.Clear()
+    $listbox.Items.AddRange($sortedItemsArray)
+}
+
+function Sort-ListBoxByLastChild {
+    param([System.Windows.Forms.ListBox]$listbox)
+
+    # Get the items in the listbox as an array
+    $itemsArray = @($listbox.Items)
+
+    # Sort the items based on the last child item in each path
+    $sortedItemsArray = $itemsArray | Sort-Object -Property { Split-Path $_ -Leaf }
+
+    # Clear the listbox and add the sorted items
+    $listbox.Items.Clear()
+    $listbox.Items.AddRange($sortedItemsArray)
+}
+
+function Sort-ListBoxByType {
+    param([System.Windows.Forms.ListBox]$listbox)
+
+    # Get the items in the listbox as an array
+    $itemsArray = @($listbox.Items)
+
+    # Sort the items by file type, with folders and items without extensions first
+    $sortedItemsArray = $itemsArray | Sort-Object -Property {
+        if ($_ -match '\.') {
+            $extension = $_ -replace '^.*\.', ''
+            if ($extension -eq $_) {
+                return '0' # For folders and items without extensions
+            }
+            return $extension
+        }
+        return '0'
+    }
+
+    # Clear the listbox and add the sorted items
+    $listbox.Items.Clear()
+    $listbox.Items.AddRange($sortedItemsArray)
+}
+
+# Create the label
+$SBlabel = New-Object System.Windows.Forms.Label
+$SBlabel.Text = "Sort By:"
+$SBlabel.Location = New-Object System.Drawing.Point(220, 75)
+
+
+# Create the dropdown menu (ComboBox)
+$dropdown = New-Object System.Windows.Forms.ComboBox
+$dropdown.Location = New-Object System.Drawing.Point(270, 75)
+$dropdown.Size = New-Object System.Drawing.Size(100, 21)
+$dropdown.DropDownStyle = [System.Windows.Forms.ComboBoxStyle]::DropDownList
+
+# Add options to the dropdown menu
+$dropdown.Items.Add("Name")
+$dropdown.Items.Add("Drive")
+$dropdown.Items.Add("Type")
+
+# Set the default selected option
+$dropdown.SelectedIndex = 0
+function Update-Sort {
+	switch ($dropdown.SelectedItem) {
+        "Drive" {
+            Sort-ListBoxAlphabetically -listbox $listbox
+        }
+		"Name" {
+            Sort-ListBoxByLastChild -listbox $listbox
+        }
+        "Type" {
+            Sort-ListBoxByType -listbox $listbox
+        }
+    }
+}
+# Handle the dropdown menu's SelectedIndexChanged event
+$dropdown.Add_SelectedIndexChanged({
+    Update-Sort
+})
+
 # Initialize form
 $form = New-Object System.Windows.Forms.Form
-$form.Text = 'Fast Search'
+$form.Text = 'Power Search'
 $form.Size = New-Object System.Drawing.Size(400, 300)
 $minWidth = 400
 $minHeight = 300
@@ -37,25 +126,68 @@ $toolTip.SetToolTip($helpIcon,(Get-Content "lua-pattern-matching.txt" -Raw))
 $textbox = New-Object System.Windows.Forms.TextBox
 $textbox.Location = New-Object System.Drawing.Point(20, 20)
 $textbox.Size = New-Object System.Drawing.Size(340, 20)
+$textbox.Add_TextChanged({
+	#run-search
+})
 $form.Controls.Add($textbox)
 
 # Initialize button
 $button = New-Object System.Windows.Forms.Button
 $button.Text = 'Search'
-$button.Location = New-Object System.Drawing.Point(270, 60)
-$button.Size = New-Object System.Drawing.Size(100, 23)
+$button.Location = New-Object System.Drawing.Point(270, 40)
+$button.AutoSize = $true
 $form.Controls.Add($button)
+# Create the sort button
+$sortButton = New-Object System.Windows.Forms.Button
+$sortButton.Location = New-Object System.Drawing.Point(350, 60)
+$sortButton.AutoSize = $true
+$sortButton.Text = "Sort Alphabetically"
+#$form.Controls.Add($sortButton)
+
+# Sort the listbox alphabetically when the button is clicked
+$sortButton.Add_Click({
+    Sort-ListBoxAlphabetically -listbox $listbox
+})
 
 $label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Point(20, 70)
+$label.Location = New-Object System.Drawing.Point(20, 60)
 $label.AutoSize = $true
 $form.Controls.Add($label)
+$processCountlabel = New-Object System.Windows.Forms.Label
+$processCountlabel.Location = New-Object System.Drawing.Point(20, 80)
+$processCountlabel.AutoSize = $true
+$form.Controls.Add($processCountlabel)
+
+$form.Controls.Add($dropdown)
+$form.Controls.Add($SBlabel)
+
+
 
 # Function to update the label with the item count
-function Update-ItemCountLabel {
-    $label.Text = "Search Results: " + $listbox.Items.Count
+function Update-ProcessCountLabel {
+	$processCount = 0
+	$newprocessCountlabelText = "Active Searches:"
+	
+	foreach ($drive in $drives) {
+        $driveLetter = $drive.Root.Substring(0, 1)
+		$isRunning = IsProcessRunning -driveLetter $driveLetter
+		if ($isRunning) {
+			$processCount += 1
+			$newprocessCountlabelText = $newprocessCountlabelText + " " + $driveLetter + ","
+		}
+	}
+	if ($processCountlabel.Text -ne $newprocessCountlabelText) {
+		$processCountlabel.Text = $newprocessCountlabelText
+	}
+	
+	
 }
-
+function Update-ItemCountLabel {
+	
+    $label.Text = "Search Results: " + $listbox.Items.Count
+   
+}
+Update-ProcessCountLabel
 # Initialize result list box
 $listbox = New-Object System.Windows.Forms.ListBox
 $listbox.Location = New-Object System.Drawing.Point(20, 100)
@@ -138,19 +270,8 @@ function Search-FilesAndFolders {
         }
 }
 
-
-function Invoke-LuaSearch {
-    param([string]$searchText, [string]$drive)
-
-    
-    
-    #$output = & $lua $luaScriptPath $searchText $drive
-	
-}
-$runningProcesses = @{} # Create an empty list to store the running processes
-$drives = Get-PSDrive -PSProvider FileSystem
-$button.Add_Click({
-    $timer.Stop()
+function run-search {
+	$timer.Stop()
 	$searchText = $textbox.Text
     $listbox.Items.Clear()
 	Update-ItemCountLabel
@@ -182,11 +303,25 @@ $button.Add_Click({
 		 $runningProcesses[$driveLetter] = $process
 	
 		while (Test-Path "GUIoutput.txt") {
-			Start-Sleep -Milliseconds 10
+			Start-Sleep -Milliseconds 1
 		}
 	}
 
     $timer.Start()
+}
+
+function Invoke-LuaSearch {
+    param([string]$searchText, [string]$drive)
+
+    
+    
+    #$output = & $lua $luaScriptPath $searchText $drive
+	
+}
+$runningProcesses = @{} # Create an empty list to store the running processes
+$drives = Get-PSDrive -PSProvider FileSystem
+$button.Add_Click({
+    run-search
 })
 function IsProcessRunning {
     param([string]$driveLetter)
@@ -242,7 +377,9 @@ $timer.Add_Tick({
 		#Resize-ListBoxWidthBasedOnLongestItem -listBoxControl $listbox
 		Update-ItemCountLabel
 		$timer.Interval = 1000+$listbox.Items.Count
+		Update-Sort
 	}
+	Update-ProcessCountLabel
 	
 })
 Update-ItemCountLabel
